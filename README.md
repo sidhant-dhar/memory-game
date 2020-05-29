b98777e4baf6b982aa35982a05e22182b40f916c
Sidhant <sdsd011235@gmail.com>
2020-05-29 17:54:34 +0530

    built memory game

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # memory-game
 A small memory game with vanilla JS.
+
+
+# How The Game Works
+The game board consists of sixteen "cards" arranged in a grid. The deck is made up of eight different pairs of cards, each with different symbols on one side. The cards are arranged randomly on the grid with the symbol face down. The gameplay rules are very simple: open two hidden cards at a time to locate the ones that match!
+
+Each turn:
+
+The player opens one card over to reveal its underlying symbol.
+The player then opens a second card, trying to find the corresponding card with the same symbol and that will be counted as one move.
+If the cards match, both cards stay opened.
+If the cards do not match, both cards are closed again.
+The stars rating depends on the number of moves:
+
+⭐⭐⭐ If the moves are between 8 and 11.
+⭐⭐ If the moves are between 12 and 19.
+⭐ If the moves are 20 or more.
+The game ends once all cards have been correctly matched. The player could restart the game at any time by clicking on the restart icon.
+
+Resources
+[Shuffle function](http://stackoverflow.com/a/2450976) from stackoverflow.
+Icons from Font [fontawesome](https://fontawesome.com/v4.7.0/icons/) - cdn.
--- js/app.js
+++ js/app.js
@@ -2,13 +2,36 @@
  * Create a list that holds all of your cards
  */
 
+const icons = ["fa fa-anchor", "fa fa-bolt", "fa fa-bomb", "fa fa-bicycle",
+"fa fa-cube", "fa fa-diamond", "fa fa-leaf", "fa fa-paper-plane-o",
+"fa fa-anchor", "fa fa-bolt", "fa fa-bomb", "fa fa-bicycle",
+"fa fa-cube", "fa fa-diamond", "fa fa-leaf", "fa fa-paper-plane-o",
+];
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+const cardsBoard = document.querySelector('#cards-board')
+const stars = document.querySelector('.stars').childNodes; 
+
+creatCardsBoard();
+
+function creatCardsBoard() {
+
+    // To clear the old card board 
+    cardsBoard.innerHTML = "";
+    // creat new ul element to append it to "cardsBoard"
+    const newDeck = document.createElement('ul');
+    newDeck.classList.add('deck');
+    // shuffle the icons list
+    let shufIcons = shuffle(icons);
+    for (let i = 0; i < shufIcons.length; i++) {
+        const newLi = document.createElement('li');
+        newLi.classList.add('card');
+        newLi.innerHTML = `<i class="${shufIcons[i]}"></i>`;
+        newDeck.appendChild(newLi);
+    }
+    cardsBoard.append(newDeck);
+
+    document.querySelector('.deck').addEventListener('click', clickResponse);
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -26,13 +49,62 @@ function shuffle(array) {
 }
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+var matchArray = [];
+var count = 0;
+var total = 8;
+function clickResponse(e) {
+    if (e.target && e.target.matches("li.card") && !e.target.classList.contains("open" , "show" , "match")) {
+        e.target.classList.add("open" , "show");
+        var matchClass = e.target;
+        setTimeout(() => {checkMatch(matchClass , matchArray)}, 800);
+        }
+    }
+ 
+
+
+
+function checkMatch(elem , matchArray) {
+    if(matchArray.length)
+        {
+       if (  matchArray[0].children[0].className === elem.children[0].className){
+          matchArray[0].classList.add("match");
+          elem.classList.add("match");
+          total--;
+          if(!total){
+            alert("Congratulations!! You have completed the game in " + count + " moves!");
+            restart();
+        }
+        } else {
+            matchArray[0].classList.remove("open" , "show");
+            elem.classList.remove("open" , "show");
+            count++;
+            document.querySelector('.moves').textContent = count;   
+            rating();  
+
+        }
+          matchArray.pop();  
+        }
+    else {
+        matchArray.push(elem);
+    }   
+}
+
+function rating() {
+       // if the moves number is between 12 and 19
+       if (count === 12) {
+        // remove third star
+        stars[5].children[0].classList.add('hide');
+        // if the moves number is 20 or more 
+    } else if (count === 20) {
+        // remove second star
+        stars[3].children[0].classList.add('hide');
+    }
+}
+
+function restart() {
+    creatCardsBoard();
+    count = 0;
+    total = 8;
+    document.querySelector('.moves').textContent = count; 
+}
+document.querySelector('.restart').addEventListener('click', restart);
